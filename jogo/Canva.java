e892b12235623db7354a7bdd742cf5149c788f62
ALEXANDRE RICOY LEITE LOPES <u15157@academico.cotuca.unicamp.br>
2017-05-24 09:19:58 -0300

    Classe para utilizar Thread

--- jogo/Canva.java
+++ jogo/Canva.java
@@ -7,28 +7,27 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.View;
 
-import java.util.Timer;
 
 /**
  * Created by u15157 on 24/05/2017.
  */
 
-public class Canva extends View {
+public class Canva extends View{
 
-    private Timer anim;
+    private Jogao game;
 
-    private Corredor[] corredores;
 
-    public Canva(Context ctx){
+    public Canva(Context ctx) {
         super(ctx);
-        anim = new Timer();
     }
 
     @Override
     public void onDraw(Canvas canvas) {
         Paint brush = new Paint();
         brush.setColor(Color.BLACK);
+        game = new Jogao(canvas);
 
-
+        game.run();
 
     }
+}
---
+++ jogo/Jogao.java
@@ -0,0 +1,21 @@
+package com.example.u15161.progetiu.jogo;
+
+import android.graphics.Canvas;
+
+/**
+ * Created by u15157 on 24/05/2017.
+ */
+
+public class Jogao extends Thread {
+    Canvas tl;
+
+    public Jogao(Canvas desenho){
+        tl = desenho;
+    }
+
+
+    @Override
+    public void run() {
+        
+    }
+}
